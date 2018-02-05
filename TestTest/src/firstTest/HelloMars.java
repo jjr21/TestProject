d48fdf4c86be79b7d4f0b7167b941be78ada5c76
jjr21 <35080838+jjr21@users.noreply.github.com>
2018-02-05 11:11:33 -0500

    Added Mars

---
+++ TestTest/src/firstTest/HelloMars.java
@@ -0,0 +1,12 @@
+package firstTest;
+
+public class HelloMars {
+
+	public static void main(String[] args) {
+		HelloMars();
+		System.exit(0);
+	}
+	public static void HelloMars() {
+		System.out.println("Hello Mars");
+	}
+}
