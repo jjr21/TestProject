19ddcccb744af27a176ff4b6e25bb74ce641d08b
jjr21 <35080838+jjr21@users.noreply.github.com>
2018-02-05 11:06:20 -0500

    First Commit

---
+++ TestTest/src/firstTest/HelloEarth.java
@@ -0,0 +1,10 @@
+package firstTest;
+
+public class HelloEarth {
+
+	public static void main(String[] args) {
+		String input = "Hello Earth";
+		System.out.println(input);
+		System.exit(0);
+	}
+}
