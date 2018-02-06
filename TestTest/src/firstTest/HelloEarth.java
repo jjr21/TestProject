96a860d194626b8a37f6c2572a24ad4770bc3123
jjr21 <35080838+jjr21@users.noreply.github.com>
2018-02-06 10:30:21 -0500

    Update Earth's file

--- TestTest/src/firstTest/HelloEarth.java
+++ TestTest/src/firstTest/HelloEarth.java
@@ -5,6 +5,10 @@ public class HelloEarth {
 	public static void main(String[] args) {
 		String input = "Hello Earth";
 		System.out.println(input);
+		Bye();
 		System.exit(0);
+	}
+	public static void Bye() {
+		System.out.println("BYE EARTH");
 	}
 }
